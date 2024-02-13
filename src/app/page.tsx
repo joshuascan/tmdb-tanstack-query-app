d00ea39fabf17f9442d64c40e759d5e2f733bacb
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2024-02-13 15:08:02 -0800

    adds hero images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,22 @@
 import Link from "next/link";
 import { css } from "../../styled-system/css";
 import Image from "next/image";
-import { center, vstack } from "../../styled-system/patterns";
+import { center, hstack, vstack } from "../../styled-system/patterns";
 
 const styledLink = css({
   color: "white",
   fontWeight: "bold",
   fontSize: "4xl",
   display: "inline-block",
-  width: 60,
-  rounded: "lg",
-  p: "3",
+  // width: 60,
+  rounded: "md",
+  // p: "3",
   bg: "#0d253f",
   textAlign: "center",
+  _hover: {
+    opacity: 0.8,
+    transition: "0.3s",
+  },
 });
 
 export default function Home() {
@@ -22,13 +26,27 @@ export default function Home() {
       <div className={center({ bg: "#0d253f", p: 12 })}>
         <Image src="/tmdb-logo.svg" width={600} height={600} alt="TMDB Logo" />
       </div>
-      <div className={vstack({ gap: 6, marginTop: 24 })}>
+      <div className={hstack({ gap: 32, marginTop: 32, justify: "center" })}>
         <Link className={styledLink} href="/movies">
-          Movies
+          <h2>Movies</h2>
+          <Image
+            className={css({ roundedBottom: "md" })}
+            src="/movie-hero.webp"
+            width={500}
+            height={500}
+            alt="movies"
+          />
         </Link>
 
         <Link className={styledLink} href="/tv">
-          TV Shows
+          <h2>TV Shows</h2>
+          <Image
+            className={css({ roundedBottom: "md" })}
+            src="/tv-hero.webp"
+            width={500}
+            height={500}
+            alt="movies"
+          />
         </Link>
       </div>
     </div>
