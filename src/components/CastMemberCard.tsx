d59465f14a5cfa166f5202a6beb99ebf4c2ded42
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2024-02-14 11:52:13 -0800

    adds spinner for cast images

--- src/components/CastMemberCard.tsx
+++ src/components/CastMemberCard.tsx
@@ -3,10 +3,31 @@ import { CastDetails } from "@/types";
 import Image from "next/image";
 import { css } from "../../styled-system/css";
 import { flex } from "../../styled-system/patterns";
+import { useState } from "react";
+import Spinner from "./Spinner";
 
 const CastMemberCard = ({ name, profile_path, character }: CastDetails) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className={flex({ direction: "column", justify: "space-between" })}>
+      {isLoading && (
+        <div
+          className={flex({
+            justify: "center",
+            align: "center",
+            height: "300px",
+            width: "200px",
+            position: "absolute",
+          })}
+        >
+          <Spinner size="sm" />
+        </div>
+      )}
       <Image
         src={
           profile_path ? `${IMAGE_URL}${profile_path}` : `${EMPTY_MOVIE_URL}`
@@ -14,6 +35,7 @@ const CastMemberCard = ({ name, profile_path, character }: CastDetails) => {
         width={200}
         height={300}
         alt={name}
+        onLoad={handleImageLoad}
       />
       <div>
         <h3 className={css({ fontSize: "lg", fontWeight: "bold" })}>{name}</h3>
