4a8d46cee40fea76594be9ac68323d8c00a95d3f
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2024-02-14 13:05:28 -0800

    creates useQuery custom hook

---
+++ src/hooks/useTMDBQuery.ts
@@ -0,0 +1,30 @@
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import fetchOptions from "@/lib/fetchOptions";
+
+type TMDBQueryParams = {
+  key: string[];
+  endpoint: string;
+  enabled?: boolean;
+};
+
+const useTMDBQuery = <T>({
+  key,
+  endpoint,
+  enabled = true,
+}: TMDBQueryParams): UseQueryResult<T, Error> => {
+  return useQuery({
+    queryKey: key,
+    queryFn: () =>
+      fetch(`https://api.themoviedb.org/3/${endpoint}`, fetchOptions).then(
+        (res) => {
+          if (!res.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return res.json();
+        }
+      ),
+    enabled,
+  });
+};
+
+export default useTMDBQuery;
